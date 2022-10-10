4c6ae08fab1498ce2e1b752859e018ec626f9e22
Brie Mengel <bmengel7@gmail.com>
2022-10-09 20:19:35 -0400

    add

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # knowledge check 1
+print('Hello World')
+
+groceries = ['Bananas', 'grapes', "soup', 'cookies"]
+print(groceries)
+
+books= {'Hunger Games': 'Suzanne Collins', 'One Flew Over the Cuckoos Nest': 'Ken Kesey', "Girl, Interrupted": 'Susanna Kaysen'}
+print(books)
+
+points_owed = (72.2, 18, 45, 159, 14)
+print(points_owed)
